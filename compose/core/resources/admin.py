01c14c7f754f80d889da48336b6d31e21c8a905d
tj <myszmen@codelab.pl>
2015-11-21 21:12:22 +0100

    views.py

--- compose/core/resources/admin.py
+++ compose/core/resources/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
-from .models import Resource
+from .models import Resource, ResourceArgument
 
-@admin.register(Resource)
+@admin.register(Resource, ResourceArgument)
 class ResourcesAdmin(admin.ModelAdmin):
     pass
