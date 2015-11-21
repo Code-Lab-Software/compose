61f59fd530ad9efcc985658b85ec9eab2e899844
Marcin Kośmider <kosmo@codelab.pl>
2015-11-21 19:59:12 +0100

    ResourceStateArgumentProvider added

--- compose/core/providers/models.py
+++ compose/core/providers/models.py
@@ -1,9 +1,17 @@
 from django.db import models
-from scopes.models import ControllerStateArgumentProvider
+from compose.core.scopes.models import ControllerStateArgumentProvider
 
 
 class ResourceStateArgumentProvider(ControllerStateArgumentProvider):
-    resource_argument = models.ForeignKey('ResourceArgument')
-
+    resource_argument = models.ForeignKey('resources.ResourceArgument')
+    
     class Meta:
         verbose_name = u'Resource state argument provider'
+
+class NoteStateArgumentProvider(ControllerStateArgumentProvider):
+    node_state = models.ForeignKey('scopes.NodeState')
+    
+    class Meta:
+        verbose_name = u'Note state argument provider'
+        verbose_name_plural = u'Note state argument providers'
+        
