e2ba813e7ba4be3897a1a1c6663cad1efeb0edc1
tj <myszmen@codelab.pl>
2015-11-21 17:44:21 +0100

    new provider: ResourceStateArgumentProvider

--- compose/core/providers/models.py
+++ compose/core/providers/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from scopes.models import ControllerStateArgumentProvider
 
-# Create your models here.
+
+class ResourceStateArgumentProvider(ControllerStateArgumentProvider):
+    resource_argument = models.ForeignKey('ResourceArgument')
+
+    class Meta:
+        verbose_name = u'Resource state argument provider'
